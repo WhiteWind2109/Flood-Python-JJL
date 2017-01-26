f7b434db4e4218ec78479fd96ec22978fe7af024
dj346 <dc2109@126.com>
2017-01-27 06:11:49 +0800

    Add functions for Task1C&1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -16,3 +16,24 @@ def stations_by_distance(stations, p):
     # Sort the list by the distances of elements to the given coordinates
     sta_dis = sorted_by_key(sta_dis, 1)
     return sta_dis
+
+def stations_within_radius(stations, centre, r):
+    ans = []
+    for station in stations:
+        if (haversine(centre, station.coord) <= r):
+            ans += station
+    return ans
+
+def rivers_with_station(stations):
+    ans = []
+    for station in stations:
+        ans.append(station.river)
+    return set(ans)
+
+def stations_by_river(stations):
+    ans = {}
+    for station in stations:
+        if (not station.river in ans):
+            ans[station.river] = []
+        ans[station.river].append(station.name)
+    return ans
