623f613de7429de221c7a3ed36f592966b068487
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-02-05 02:58:28 +0000

    Added the required docstrings for functions in "geo.py"
    and minor changes & annotations

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,8 +9,13 @@ from haversine import haversine
 
 
 def stations_by_distance(stations, p):
+    """Given a list of stations and a coordinate p,
+    returns a list of (station, distance) tuples,
+    where distance (float) is the distance of the station (MonitoringStation) from the coordinate p.
+    The returned list is sorted by distance."""
+
     # Create the list of tuple to be returned
-    ans = list()
+    ans = []
     for station in stations:
         distance = haversine(p, station.coord)
         ans.append((station, distance))
@@ -20,6 +25,9 @@ def stations_by_distance(stations, p):
 
 
 def stations_within_radius(stations, centre, r):
+    """Returns a list of all stations (type MonitoringStation)
+    within radius r of a geographic coordinate x."""
+
     # Create the list of stations to be returned
     ans = []
     for station in stations:
@@ -29,6 +37,8 @@ def stations_within_radius(stations, centre, r):
 
 
 def rivers_with_station(stations):
+    """Given a list of stations, returns all rivers (by name) with a monitoring station."""
+
     # Create the list of rivers then change its type to set to get unique
     # elements
     ans = []
@@ -38,6 +48,9 @@ def rivers_with_station(stations):
 
 
 def stations_by_river(stations):
+    """Returns a Python dict (dictionary) that maps river names (the ‘key’)
+    to a list of stations on a given river."""
+
     # Create the dictionary of river->[station] to be returned
     ans = {}
     for station in stations:
@@ -49,14 +62,25 @@ def stations_by_river(stations):
 
 
 def rivers_by_station_number(stations, N):
-    ans = list()
+    """Determines the N rivers with the greatest number of monitoring stations.
+    It returns a list of (river name, number of stations) tuples,
+    sorted by the number of stations.
+    In the case that there are more rivers with the same number of stations as the N th entry,
+    these rivers are included in the list."""
+
+    # First, get a complete list of (river name, number of stations) tuples
+    complete_list = []
     stations_on_river = stations_by_river(stations)
     for river in stations_on_river:
-        ans.append((river, len(stations_on_river[river])))
-    ans = sorted_by_key(ans, 1, reverse=True)
-    threshold = ans[N - 1][1]
-    greatest_N = list()
-    for river in ans:
+        complete_list.append((river, len(stations_on_river[river])))
+
+    # Now sort it by numbers of stations in descending order
+    complete_list = sorted_by_key(complete_list, 1, reverse=True)
+
+    # The threshold value: the N-th greatest value
+    threshold = complete_list[N - 1][1]
+    greatest_N = []
+    for river in complete_list:
         if river[1] >= threshold:
             greatest_N.append((river))
     return greatest_N
