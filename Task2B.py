d2fb1e33f7f5df3152685d5f9b74b5bd0aaa77f2
dj346 <dc2109@126.com>
2017-02-07 06:37:42 +0800

    Correct a minor typo

--- Task2B.py
+++ Task2B.py
@@ -20,6 +20,6 @@ def run():
 
 
 if __name__ == "__main__":
-    print("*** Task 2: CUED Part IA Flood Warning System ***")
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
 
     run()
