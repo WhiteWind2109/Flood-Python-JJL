4421408ec059f02a08b11bedd461acae134f87bc
dj346 <dc2109@126.com>
2017-02-07 06:38:41 +0800

    pytest for the new module: flood

---
+++ test_flood.py
@@ -0,0 +1,13 @@
+"""Unit test for the flood module"""
+
+import pytest
+from floodsystem.stationdata import build_station_list
+
+def test_stations_level_over_threshold():
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    present = flood.stations_level_over_threshold(stations, 0.8)
+    assert present[0][1] > 0.8
