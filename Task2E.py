5702f0c52d2a5ffe64ca1565220ae147fc47d83a
dj346 <dc2109@126.com>
2017-02-07 20:55:16 +0800

    Task2E interesting plotting

---
+++ Task2E.py
@@ -0,0 +1,29 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem import flood
+from floodsystem import datafetcher
+from floodsystem import plot
+import datetime
+
+def run():
+    """Requirements for Task2E"""
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    shortlist = flood.stations_highest_rel_level(stations, 5)
+
+    for station in shortlist:
+        dt = 10
+        for target in stations:
+            if target.name == station[0]:
+                dates, levels = datafetcher.fetch_measure_levels(target.measure_id,
+                                                    dt=datetime.timedelta(days=dt))
+                plot.plot_water_levels(target, dates, levels)
+                break
+
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+
+    run()
