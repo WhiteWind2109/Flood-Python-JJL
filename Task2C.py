94e221069ab5e64d18e7ebd07aa2d94bc2908419
dj346 <dc2109@126.com>
2017-02-07 19:51:35 +0800

    Task2C almost finished

---
+++ Task2C.py
@@ -0,0 +1,23 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem import flood
+
+def run():
+    """Requirements for Task2C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    # Define N
+    N = 10
+
+    shortlist = flood.stations_highest_rel_level(stations, N)
+
+    for station in shortlist:
+        print("{} {}".format(station[0], station[1]))
+
+
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+
+    run()
