2e7696233a0f2ae9f0f8c2864af02cc06e4c1fa1
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-02-19 17:56:55 +0000

    Task2C docstrings and annotations added

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -5,7 +5,8 @@ from .utils import sorted_by_key
 
 
 def stations_level_over_threshold(stations, tol):
-    """Returns a list of tuples, where each tuple holds:
+    """stations_level_over_threshold(stations, tol) --
+    Returns a list of tuples, where each tuple holds:
     (1) a station at which the latest relative water level is over tol and
     (2) the relative water level at the station. The returned list should
     be sorted by the relative level in descending order.
@@ -34,15 +35,26 @@ def stations_level_over_threshold(stations, tol):
 
 
 def stations_highest_rel_level(stations, N):
-    """Given a list of stations and a number N
-        return a list of N stations with their relative levels in descending order"""
+    """stations_highest_rel_level(stations, N) --
+    Returns a list of the N stations at which the water level, relative to the typical range,
+    is highest. The list is sorted in descending order by relative level."""
 
     ans = []
     for station in stations:
-        # Still about the type problem (need modifications in station.py?)
         if isinstance(station.latest_level, float):
+
+            # Store the returned value to avoid repetitive calls
+            # to slightly improve performance
             r_level = station.relative_water_level()
+
+            # Only valid data can be compared by sorted_by_key()
             if r_level is not None:
-                ans.append((station, station.relative_water_level()))
+
+                # Make a decorated list
+                ans.append((station, r_level))
+
+    # Sort the decorated list by relative water level
     ans = sorted_by_key(ans, 1, reverse=True)
-    return [station for station, level in ans[:N]]
+
+    # Return the undecorated list
+    return [station for station, rel_level in ans[:N]]
